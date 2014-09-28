d2d1a5122d27af5f085bc86826216532454e0bf1
Takahisa Kawando <kawando@gmail.com>
2014-09-29 01:04:58 +0900

    種別毎に色を変更（色は後で調整）

--- js/ol_style.js
+++ js/ol_style.js
@@ -1,94 +1,23 @@
-/*
-var image = new ol.style.Circle({
-  radius: 10,
-  fill: new ol.style.Fill({
-      color: 'rgba(255, 0, 0, 1)'
-  }),
-  stroke: new ol.style.Stroke({color: 'red', width: 1})
-});
-
-var styles = {
-  'Point': [new ol.style.Style({
-    image: image
-  })],
-  'LineString': [new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'green',
-      width: 1
-    })
-  })],
-  'MultiLineString': [new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'green',
-      width: 1
-    })
-  })],
-  'MultiPoint': [new ol.style.Style({
-    image: image
-  })],
-  'MultiPolygon': [new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'yellow',
-      width: 1
-    }),
-    fill: new ol.style.Fill({
-      color: 'rgba(255, 255, 0, 0.1)'
-    })
-  })],
-  'Polygon': [new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'blue',
-      lineDash: [4],
-      width: 3
-    }),
-    fill: new ol.style.Fill({
-      color: 'rgba(0, 0, 255, 0.1)'
-    })
-  })],
-  'GeometryCollection': [new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'magenta',
-      width: 2
-    }),
-    fill: new ol.style.Fill({
-      color: 'magenta'
-    }),
-    image: new ol.style.Circle({
-      radius: 10,
-      fill: null,
-      stroke: new ol.style.Stroke({
-        color: 'magenta'
-      })
-    })
-  })],
-  'Circle': [new ol.style.Style({
-    stroke: new ol.style.Stroke({
-      color: 'red',
-      width: 2
-    }),
-    fill: new ol.style.Fill({
-      color: 'rgba(255,0,0,0.2)'
-    })
-  })]
-};
-
-var styleFunction = function(feature, resolution) {
-  return styles[feature.getGeometry().getType()];
-};
-*/
+function getColor(type) {
+	if ('認可保育所' == type){
+		return 'rgba(0, 255, 0, 1)';
+	} else if ('認可外保育所' == type){
+		return 'rgba(0, 0, 255, 1)';
+	} else if ('幼稚園' == type){
+		return 'rgba(255, 0, 255, 1)';
+	} else if ('認定こども園' == type){
+		return 'rgba(255, 255, 0, 1)';
+	}
+	return 'rgba(153, 153, 153, 1)';
+}
 // 保育所スタイル
 var nurseryStyleFunction = function(feature, resolution) {
 	var radius = 10;
 	var type = feature.get('種別');
-	if ('認可保育所' == type){
-		radius = 8;
-	} else if ('認可外保育所' == type){
-		radius = 6;
-	}
 	var image = new ol.style.Circle({
 		radius: radius,
 		fill: new ol.style.Fill({
-			color: 'rgba(0, 255, 0, 1)'
+			color: getColor(type)
 		}),
 		stroke: new ol.style.Stroke({color: 'black', width: 1})
 	});
